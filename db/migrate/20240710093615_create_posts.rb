7587ed60d777ad9f6c6b6f72bc646f44ebccb567
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-07-14 12:33:54 +0000

    [Fix] postの初期値を取りやめ

--- db/migrate/20240710093615_create_posts.rb
+++ db/migrate/20240710093615_create_posts.rb
@@ -8,9 +8,9 @@ class CreatePosts < ActiveRecord::Migration[6.1]
         # 会員(書店)ID
         t.integer :book_store_id, null: false
         # タイトル
-        t.text :title, null: false, default: ""
+        t.text :title, null: false
         # 投稿内容
-        t.text :content, null: false, default: ""
+        t.text :content, null: false
 
     end
   end
